e0b8358fc8900fd1129ff4619000d49806d0e89e
Israel Vieira Ribeiro <israel.ribeiro@everymind.com.br>
2022-08-14 12:46:10 -0300

    Criando as primeiras classes

---
+++ ContasClientes/ContasClientes/Models/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContasClientes.Models
+{
+     class Cliente
+    {
+        private long Id { get; set; }
+        private string Nome { get; set; }
+        private string Cpf { get; set; }
+
+        public Cliente(long id, string nome, string cpf)
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.Cpf = cpf;
+        }
+    }
+}
